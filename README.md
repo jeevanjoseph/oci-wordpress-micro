10d0fad00a6873155a28bec5769fe066ad66fe3a
Jeevan Joseph <jeevan.g@gmail.com>
2021-01-22 13:59:05 -0800

    removing extra networking resources

--- README.md
+++ README.md
@@ -1,32 +1,15 @@
-# oci-arch-wordpress-mds
+# oci-wordpress-micro
 
 ## Prerequisites
 
-- Permission to `manage` the following types of resources in your Oracle Cloud Infrastructure tenancy: `vcns`, `internet-gateways`, `route-tables`, `security-lists`, `subnets`, `mysql-family`, and `instances`.
+- Permission to `manage` the following types of resources in your Oracle Cloud Infrastructure tenancy: `vcns`, `internet-gateways`, `route-tables`, `security-lists`, `subnets` and `instances`.
 
-- Quota to create the following resources: 1 VCN, 2 subnets, 1 Internet Gateway, 1 NAT Gateway, 2 route rules, 1 MDS instance, and 1 compute instance (Wordpress CMS).
+- Quota to create the following resources: 1 VCN, 1 subnet, 1 Internet Gateway, and 1 compute instance (Wordpress CMS).
 
 If you don't have the required permissions and quota, contact your tenancy administrator. See [Policy Reference](https://docs.cloud.oracle.com/en-us/iaas/Content/Identity/Reference/policyreference.htm), [Service Limits](https://docs.cloud.oracle.com/en-us/iaas/Content/General/Concepts/servicelimits.htm), [Compartment Quotas](https://docs.cloud.oracle.com/iaas/Content/General/Concepts/resourcequotas.htm).
 
 ## Deploy Using Oracle Resource Manager
 
-1. Click [![Deploy to Oracle Cloud](https://oci-resourcemanager-plugin.plugins.oci.oraclecloud.com/latest/deploy-to-oracle-cloud.svg)](https://console.us-phoenix-1.oraclecloud.com/resourcemanager/stacks/create?region=home&zipUrl=https://github.com/oracle-quickstart/oci-arch-wordpress-mds/raw/master/resource-manager/oci-arch-wordpress-mds.zip)
-
-    If you aren't already signed in, when prompted, enter the tenancy and user credentials.
-
-2. Review and accept the terms and conditions.
-
-3. Select the region where you want to deploy the stack.
-
-4. Follow the on-screen prompts and instructions to create the stack.
-
-5. After creating the stack, click **Terraform Actions**, and select **Plan**.
-
-6. Wait for the job to be completed, and review the plan.
-
-    To make any changes, return to the Stack Details page, click **Edit Stack**, and make the required changes. Then, run the **Plan** action again.
-
-7. If no further changes are necessary, return to the Stack Details page, click **Terraform Actions**, and select **Apply**. 
 
 ## Deploy Using the Terraform CLI
 
@@ -57,33 +40,6 @@ region = "<oci_region>"
 
 # Availablity Domain 
 availablity_domain_name = "<availablity_domain_name>"
-
-# Compartment
-compartment_ocid = "<compartment_ocid>"
-
-# MySQL DBSystem Admin Password 
-admin_password = "<admin_password>"
-
-# WordPress MySQL Database User Password
-wp_password = "<wp_user_password>"
-
-# WordPress Themes to be installed by WP-CLI (minimum 1 theme)
-wp_themes = "lodestar,twentysixteen"
-
-# WordPress Plugins to be installed by WP-CLI (minimum 1 plugin)
-wp_plugins = "hello-dolly,elementor"
-
-# WordPress Site Title
-wp_site_title = "<WordPress_site_title>"
-       
-# WordPress WP-Admin User
-wp_site_admin_user = "<wp-admin_user>"
-
-# WordPress WP-Admin Password
-wp_site_admin_pass = "<wp-admin_password>"
-
-# WordPress WP-Admin e-mail address
-wp_site_admin_email = "<wp-admin_email>"
 
 ````
 
--- network.tf
+++ network.tf
@@ -2,7 +2,7 @@ resource "oci_core_virtual_network" "wpmdsvcn" {
   cidr_block = var.vcn_cidr
   compartment_id = var.compartment_ocid
   display_name = var.vcn
-  dns_label = "wpmdsvcn"
+  dns_label = "wordpress"
 }
 
 
@@ -12,33 +12,16 @@ resource "oci_core_internet_gateway" "internet_gateway" {
   vcn_id = oci_core_virtual_network.wpmdsvcn.id
 }
 
-
-resource "oci_core_nat_gateway" "nat_gateway" {
-  compartment_id = var.compartment_ocid
-  vcn_id         = oci_core_virtual_network.wpmdsvcn.id
-  display_name   = "nat_gateway"
-}
-
-
 resource "oci_core_route_table" "public_route_table" {
   compartment_id = var.compartment_ocid
   vcn_id = oci_core_virtual_network.wpmdsvcn.id
-  display_name = "RouteTableForMySQLPublic"
+  display_name = "RouteTableForWordPress"
   route_rules {
     cidr_block = "0.0.0.0/0"
     network_entity_id = oci_core_internet_gateway.internet_gateway.id
   }
 }
 
-resource "oci_core_route_table" "private_route_table" {
-  compartment_id = var.compartment_ocid
-  vcn_id         = oci_core_virtual_network.wpmdsvcn.id
-  display_name   = "RouteTableForMySQLPrivate"
-  route_rules {
-    destination       = "0.0.0.0/0"
-    network_entity_id = oci_core_nat_gateway.nat_gateway.id
-  }
-}
 
 resource "oci_core_security_list" "public_security_list" {
   compartment_id = var.compartment_ocid
@@ -84,67 +67,13 @@ resource "oci_core_security_list" "public_security_list_http" {
   }
 }
 
-resource "oci_core_security_list" "private_security_list" {
-  compartment_id = var.compartment_ocid
-  display_name   = "Private"
-  vcn_id         = oci_core_virtual_network.wpmdsvcn.id
-
-  egress_security_rules {
-    destination = "0.0.0.0/0"
-    protocol    = "all"
-  }
-  ingress_security_rules  {
-    protocol = "1"
-    source   = var.vcn_cidr
-  }
-  ingress_security_rules  {
-    tcp_options  {
-      max = 22
-      min = 22
-    }
-    protocol = "6"
-    source   = var.vcn_cidr
-  }
-  ingress_security_rules  {
-    tcp_options  {
-      max = 3306
-      min = 3306
-    }
-    protocol = "6"
-    source   = var.vcn_cidr
-  }
-  ingress_security_rules  {
-    tcp_options  {
-      max = 33061
-      min = 33060
-    }
-    protocol = "6"
-    source   = var.vcn_cidr
-  }
-}
-
 resource "oci_core_subnet" "public" {
   cidr_block = cidrsubnet(var.vcn_cidr, 8, 0)
-  display_name = "wp_public_subnet"
+  display_name = "wordpress_public_subnet"
   compartment_id = var.compartment_ocid
   vcn_id = oci_core_virtual_network.wpmdsvcn.id
   route_table_id = oci_core_route_table.public_route_table.id
   security_list_ids = [oci_core_security_list.public_security_list.id, oci_core_security_list.public_security_list_http.id]
   dhcp_options_id = oci_core_virtual_network.wpmdsvcn.default_dhcp_options_id
-  dns_label = "wppub"
+  dns_label = "wp"
 }
-
-resource "oci_core_subnet" "private" {
-  cidr_block                 = cidrsubnet(var.vcn_cidr, 8, 1)
-  display_name               = "mds_private_subnet"
-  compartment_id             = var.compartment_ocid
-  vcn_id                     = oci_core_virtual_network.wpmdsvcn.id
-  route_table_id             = oci_core_route_table.private_route_table.id
-  security_list_ids          = [oci_core_security_list.private_security_list.id]
-  dhcp_options_id            = oci_core_virtual_network.wpmdsvcn.default_dhcp_options_id
-  prohibit_public_ip_on_vnic = "true"
-  dns_label                  = "mdspriv"
-}
-
-
-
